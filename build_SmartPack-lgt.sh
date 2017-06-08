961125ac2185e8c0790e5ac52b8adc5e22dff629
sunilpaulmathew <sunil.kde@gmail.com>
2017-06-08 20:41:50 +0900

    build_SmartPack-lgt.sh: some modifications
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_SmartPack-lgt.sh
+++ build_SmartPack-lgt.sh
@@ -9,20 +9,20 @@ export CROSS_COMPILE=/home/sunil/arm-linux-androideabi-4.9-linaro/bin/arm-linux-
 
 mkdir output_lgt
 
-# building Smartpack kernel for kltelgt
+echo -e $COLOR_GREEN"\n building Smartpack kernel for kltelgt\n"$COLOR_NEUTRAL
 
 make -C $(pwd) O=output_lgt msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_lgt_defconfig SELINUX_DEFCONFIG=selinux_defconfig && make -C $(pwd) O=output_lgt
 
-# copying zImage to anykernel directory
+echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
 
 cp output_lgt/arch/arm/boot/zImage anykernel_SmartPack/
 
-# generating recovery flashable zip file
+echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 
 cd anykernel_SmartPack/ && zip -r9 SmartPack_kernel_kltelgt_$(date +"%Y%m%d").zip * -x README SmartPack_kernel_kltelgt_$(date +"%Y%m%d").zip && cd ..
 
-# cleaning...
+echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
 
-rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack/ && rm -r output_*
+rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack/
 
-# everything done... please visit "release_SmartPack"...
+echo -e $COLOR_GREEN"\n everything done... please visit "release_SmartPack"...\n"$COLOR_NEUTRAL

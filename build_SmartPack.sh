db67a4fa237d2366591de9cf9a3111fa25e6c94e
sunilpaulmathew <sunil.kde@gmail.com>
2017-06-09 09:01:31 +0900

    build_SmartPack.sh: some modifications
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_SmartPack.sh
+++ build_SmartPack.sh
@@ -1,92 +1,97 @@
 #!/bin/bash
 
-# SmartPack Kernel Build Script
+echo -e $COLOR_GREEN"\n SmartPack Kernel Build Script\n"$COLOR_NEUTRAL
 #
-# (c) sunilpaulmathew@xda-developers.com
+echo -e $COLOR_GREEN"\n (c) sunilpaulmathew@xda-developers.com\n"$COLOR_NEUTRAL
 
-export ARCH=arm
-export CROSS_COMPILE=/home/sunil/arm-linux-androideabi-4.9-linaro/bin/arm-linux-androideabi-
+TOOLCHAIN="/home/sunil/arm-linux-androideabi-4.9-linaro/bin/arm-linux-androideabi-"
+ARCHITECTURE=arm
+
+NUM_CPUS=""   # number of cpu cores used for build (leave empty for auto detection)
+
+export ARCH=$ARCHITECTURE
+export CROSS_COMPILE="${CCACHE} $TOOLCHAIN"
 
 mkdir output_kor output_eur output_duos output_spr output_dv
 
-# building Smartpack kernel for kltekor
+echo -e $COLOR_GREEN"\n building Smartpack kernel for kltekor\n"$COLOR_NEUTRAL
 
 make -C $(pwd) O=output_kor msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_kor_defconfig SELINUX_DEFCONFIG=selinux_defconfig && make -C $(pwd) O=output_kor
 
-# copying zImage to anykernel directory
+echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
 
 cp output_kor/arch/arm/boot/zImage anykernel_SmartPack/
 
-# generating recovery flashable zip file
+echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 
 cd anykernel_SmartPack/ && zip -r9 SmartPack_kernel_kltekor_$(date +"%Y%m%d").zip * -x README SmartPack_kernel_kltekor_$(date +"%Y%m%d").zip && cd ..
 
-# cleaning...
+echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
 
 rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/dtb release_SmartPack && mv anykernel_SmartPack/SmartPack_* release_SmartPack/
 
-# building Smartpack kernel for klte
+echo -e $COLOR_GREEN"\n building Smartpack kernel for klte\n"$COLOR_NEUTRAL
 
 make -C $(pwd) O=output_eur msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig && make -C $(pwd) O=output_eur
 
-# copying zImage to anykernel directory
+echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
 
 cp output_eur/arch/arm/boot/zImage anykernel_SmartPack/
 
-# generating recovery flashable zip file
+echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 
 cd anykernel_SmartPack/ && zip -r9 SmartPack_kernel_klte_$(date +"%Y%m%d").zip * -x README SmartPack_kernel_klte_$(date +"%Y%m%d").zip && cd ..
 
-# cleaning...
+echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
 
 rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack/
 
-# building Smartpack kernel for klteduos
+echo -e $COLOR_GREEN"\n building Smartpack kernel for klteduos\n"$COLOR_NEUTRAL
 
 make -C $(pwd) O=output_duos msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_duos_defconfig SELINUX_DEFCONFIG=selinux_defconfig && make -C $(pwd) O=output_duos
 
-# copying zImage to anykernel directory
+echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
 
 cp output_duos/arch/arm/boot/zImage anykernel_SmartPack/
 
-# generating recovery flashable zip file
+echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 
 cd anykernel_SmartPack/ && zip -r9 SmartPack_kernel_klteduos_$(date +"%Y%m%d").zip * -x README SmartPack_kernel_klteduos_$(date +"%Y%m%d").zip && cd ..
 
-# cleaning...
+echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
 
 rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack/
 
-# building Smartpack kernel for kltespr
+echo -e $COLOR_GREEN"\n building Smartpack kernel for kltespr\n"$COLOR_NEUTRAL
 
 make -C $(pwd) O=output_spr msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_spr_defconfig SELINUX_DEFCONFIG=selinux_defconfig && make -C $(pwd) O=output_spr
 
-# copying zImage to anykernel directory
+echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
 
 cp output_spr/arch/arm/boot/zImage anykernel_SmartPack/
 
-# generating recovery flashable zip file
+echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 
 cd anykernel_SmartPack/ && zip -r9 SmartPack_kernel_kltespr_$(date +"%Y%m%d").zip * -x README SmartPack_kernel_kltespr_$(date +"%Y%m%d").zip && cd ..
 
-# cleaning...
+echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
 
 rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack/
 
-# building Smartpack kernel for kltedv
+echo -e $COLOR_GREEN"\n building Smartpack kernel for kltedv\n"$COLOR_NEUTRAL
 
 make -C $(pwd) O=output_dv msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_dv_defconfig SELINUX_DEFCONFIG=selinux_defconfig && make -C $(pwd) O=output_dv
 
-# copying zImage to anykernel directory
+echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
 
 cp output_dv/arch/arm/boot/zImage anykernel_SmartPack/
 
-# generating recovery flashable zip file
+echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 
 cd anykernel_SmartPack/ && zip -r9 SmartPack_kernel_kltedv_$(date +"%Y%m%d").zip * -x README SmartPack_kernel_kltedv_$(date +"%Y%m%d").zip && cd ..
 
-# cleaning...
+echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
 
-rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack && mv release_SmartPack/dtb anykernel_SmartPack/ && rm -r output_*
+rm anykernel_SmartPack/zImage && mv anykernel_SmartPack/SmartPack_* release_SmartPack && mv release_SmartPack/dtb anykernel_SmartPack/
 
-# everything done... please visit "release_SmartPack"...
+echo -e $COLOR_GREEN"\n everything done... please visit "release_SmartPack"...\n"$COLOR_NEUTRAL
